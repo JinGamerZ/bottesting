38ab3182f8d4953b4b238909a51daee1897121a9
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-04-02 22:59:04 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on("ready", () => {
 });
 
 client.on('ready', () => {
-  client.user.setActivity('with fire', {type: 1})
+  client.user.setActivity('with fire', {type: 2})
 })
 
 client.on('guildDelete', guild => {
